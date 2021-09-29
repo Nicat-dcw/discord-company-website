d4471a22aa8a93fa3dc622822bb73754f38dfee8
Glitch (hello-express) <none>
2021-09-29 18:56:51 +0000

    🦄💂 Checkpoint ./src/web/main.js:33044891/718 ./src/web/sayfa/index.ejs:33044891/1 ./package.json:33044891/1390

--- src/web/main.js
+++ src/web/main.js
@@ -1,6 +1,8 @@
 const express = require("express");
 //const bodyParser = require("body-parser") 
 const app = express();
+const path = require("path");
+//const client = require("nicatdcw.js") 
 
 app.set('view engine', 'ejs');
 
@@ -9,11 +11,34 @@ var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const dataDir = path.resolve(`${process.cwd()}${path.sep}web`);
+
+  const templateDir = path.resolve(`${dataDir}${path.sep}sayfa${path.sep}`);
+
 
 
 app.set('view engine', 'ejs')
 /*==Sayfa==*/
+const renderr = (res, req, template, data = {}) => {
+
+    const baseData = {
+
+      //bot: client,
+
+      path: req.path,
+
+      user: req.isAuthenticated() ? req.user : null
+
+    };
+
+    res.render(path.resolve(`${templateDir}${path.sep}${template}`), Object.assign(baseData, data));
+
+  };
 app.get("/", (req, res) => {
-  res.render("index")
+  renderr("index")
  });
+app.get("/company", (req, res) => {
+  res.render("company")
+ });
+app.listen(8080)
         
